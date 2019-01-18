00e303a3476a8330276025175b36e616485d9cb7
XxingY <1391065515@qq.com>
2019-01-18 19:32:32 +0800

    换了个高清视频

--- js/index.js
+++ js/index.js
@@ -18,9 +18,69 @@
 
 
 $(function () {
-    let flag = true
-    numIsShow()
-    //监听滚动事件
+    var times_id, degree_id, min_id, gun_id
+    //滚动数字
+    function scrollNum() {
+        let f = 100
+        let avg = 105
+        let count_times = 0
+        let count_degree = 0
+        let count_min = 0
+        let count_gun = 0
+        let max_times = 420
+        let max_degree = 10670
+        let max_min = 31235
+        let max_gun = 200
+        times_id = setInterval(function () {
+            count_times += Math.ceil(max_times / avg)
+            if (count_times > max_times) {
+                times_id = clearInterval(times_id)
+                count_times = max_times
+            }
+            $("#count_times").html(count_times)
+        }, f)
+        degree_id = setInterval(function () {
+            count_degree += Math.ceil(max_degree / avg)
+            if (count_degree > max_degree) {
+                degree_id = clearInterval(degree_id)
+                count_degree = max_degree
+            }
+            $("#count_degree").html(count_degree)
+        }, f)
+        min_id = setInterval(function () {
+            count_min += Math.ceil(max_min / avg)
+            if (count_min > max_min) {
+                min_id = clearInterval(min_id)
+                count_min = max_min
+            }
+            $("#count_min").html(count_min)
+        }, f)
+        gun_id = setInterval(function () {
+            count_gun += Math.ceil(max_gun / avg)
+            if (count_gun > max_gun) {
+                count_gun = max_gun
+                gun_id = clearInterval(gun_id)
+            }
+            $("#count_gun").html(count_gun)
+        }, f)
+    }
+
+    let flag = true//标记数字第一次出现
+    //第一次拉到数字出现的地方开始滚动
+    function numIsShow() {
+        let t = document.documentElement.scrollTop || document.body.scrollTop
+        let numText = document.querySelector('.number')
+        //数字中线出现在视口
+        if (t + window.innerHeight >= (numText.offsetTop + numText.offsetHeight / 2)) {
+            if (flag) {
+                scrollNum()//数字开始滚动
+                flag = false
+            }
+        }
+    }
+    numIsShow()//刷新页面看到数字也开始滚动
+
+    //监听鼠标滚动，调整顶部导航颜色
     window.onscroll = function () {
         let t = document.documentElement.scrollTop || document.body.scrollTop
         let navList = document.getElementsByClassName("top-nav")
@@ -36,71 +96,13 @@ $(function () {
         numIsShow()
     }
 
-    function numIsShow() {
-        let t = document.documentElement.scrollTop || document.body.scrollTop
-        let numText = document.querySelector('.number')
-        if (t + window.innerHeight >= (numText.offsetTop + numText.offsetHeight / 2)) {
-            if (flag) {
-                scrollNum()//数字开始滚动
-                flag = false
-            }
+    //监听鼠标进入数字区域，让数字重新滚动
+    var area = document.querySelector('#numArea')
+    area.addEventListener('mouseenter', function () {
+        if (times_id || degree_id || min_id || gun_id) {
+        } else {
+            scrollNum()
         }
-    }
-
-    //动态数字
-    function scrollNum() {
-        let f = 100
-        let count_times = 0
-        let count_degree = 0
-        let count_min = 0
-        let count_gun = 0
-        let times_id = setInterval(function () {
-            count_times += parseInt(Math.random() * 3) + 1
-            $("#count_times").html(count_times)
-            if (count_times > 420) {
-                clearInterval(times_id)
-                times_id = setInterval(function () {
-                    count_times += parseInt(Math.random() * 5) + 1
-                    $("#count_times").html(count_times)
-                }, 60000)
-            }
-        }, f)
-        let degree_id = setInterval(function () {
-            count_degree += parseInt(Math.random() * 100) + 1
-            $("#count_degree").html(count_degree)
-            if (count_degree > 10670) {
-                clearInterval(degree_id)
-                degree_id = setInterval(function () {
-                    count_degree += parseInt(Math.random() * 100) + 1
-                    $("#count_degree").html(count_degree)
-                }, 60000)
-            }
-        }, f)
-        let min_id = setInterval(function () {
-            count_min += parseInt(Math.random() * 300) + 1
-            $("#count_min").html(count_min)
-            if (count_min > 31235) {
-                clearInterval(min_id)
-                min_id = setInterval(function () {
-                    count_min += parseInt(Math.random() * 60) + 1
-                    $("#count_min").html(count_min)
-                }, 60000)
-            }
-        }, f)
-        let gun_id = setInterval(function () {
-            count_gun += parseInt(Math.random()) + 1
-            if (count_gun > 200) {
-                count_gun = 200
-                clearInterval(gun_id)
-            }
-            $("#count_gun").html(count_gun)
-        }, f)
-    }
-
-    changeVideoSize()
-    //监听窗口大小变化
-    $(window).resize(function () {
-        changeVideoSize()
     })
 
     //调整视频大小
@@ -116,8 +118,13 @@ $(function () {
             }
         }
     }
+    changeVideoSize()
+    //监听窗口大小变化，适应视频大小
+    $(window).resize(function () {
+        changeVideoSize()
+    })
 
-    //切换视频文字，调整导航条文字颜色
+    //监听视频加载完成，切换视频文字，调整顶部导航条文字颜色
     var videoEle = document.querySelector("#videoEle")
     var timeId = 0
     videoEle.oncanplay = function () {
@@ -141,9 +148,8 @@ $(function () {
                 mail.attr('src', './images/mail1.png')
                 navList[0].style.color = "#000000"
             }
-        }, 7000)
+        }, 5000)
 
     }
-
 
 })
